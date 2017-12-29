13e14b8e7f26435f53a94b77cc15ec78d5ac8d7f
Sagar <32057132+Sagard89@users.noreply.github.com>
2017-12-29 18:36:30 +0530

    Add files via upload

---
+++ src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css','../assets/css/application-startup.css','../assets/css/toolkit-startup.css']
+})
+
+export class AppComponent {
+  title = 'app';
+  
+  
+}
